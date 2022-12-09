01ec0852374659a039867bcaf3b4b0434f073659
okawaffles <okawaffles@gmail.com>
2022-12-09 12:34:16 -0600

    refactor coordinates.java to be neater

--- src/main/java/com/okawaffles/helpfulwaffleplugin/commands/coordinates.java
+++ src/main/java/com/okawaffles/helpfulwaffleplugin/commands/coordinates.java
@@ -8,47 +8,44 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-/*
-* todo:
-*  - refactor code (i.e. get rid of if/else statements where possible)
-* */
-
 public class coordinates implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player plr0 = (Player) sender;
+        if (!(sender instanceof Player)) return false;
+        Player plr0 = (Player) sender;
 
-            double plrX = plr0.getLocation().getX();
-            double plrY = plr0.getLocation().getY();
-            double plrZ = plr0.getLocation().getZ();
+        if (args.length == 0) {
+            plr0.sendMessage(ChatColor.DARK_RED + "[!] Please specify one of the following options: broadcast, tell");
+            return false;
+        }
 
-            String myCoordinates = Math.round(plrX) +" / "+ Math.round(plrY) +" / "+ Math.round(plrZ);
+        double plrX = plr0.getLocation().getX();
+        double plrY = plr0.getLocation().getY();
+        double plrZ = plr0.getLocation().getZ();
+        String myCoordinates = Math.round(plrX) +" / "+ Math.round(plrY) +" / "+ Math.round(plrZ);
 
-            if (args.length > 0) {
-                switch(args[0]) {
-                    case "broadcast":
-                        Bukkit.broadcastMessage(ChatColor.DARK_AQUA + "[~] " + plr0.getDisplayName() + "'s coordinates are: " + myCoordinates);
-                        break;
-                    case "tell":
-                        Player sendTo;
-                        if (args.length > 1) {
-                            sendTo = Bukkit.getPlayer(args[1]);
-
-                            plr0.sendMessage(ChatColor.DARK_GREEN + "[~ To " + sendTo.getDisplayName() + "] My coordinates are " + myCoordinates + "! ");
-                            sendTo.sendMessage(ChatColor.DARK_AQUA + "[~ From " + sendTo.getDisplayName() + "] My coordinates are " + myCoordinates + "! ");
-                            sendTo.playSound(sendTo.getLocation(), Sound.BLOCK_NOTE_BLOCK_BIT, 20F, 1F);
-                        } else plr0.sendMessage(ChatColor.DARK_RED + "[!] Please provide a player!");
-                        break;
-                    default:
-                        plr0.sendMessage(ChatColor.DARK_RED + "[!] Please specify one of the following options: broadcast, tell");
-                        break;
+        switch(args[0]) {
+            case "broadcast":
+                Bukkit.broadcastMessage(ChatColor.DARK_AQUA + "[~] " + plr0.getDisplayName() + "'s coordinates are: " + myCoordinates);
+                break;
+            case "tell":
+                if (args.length < 2) {
+                    plr0.sendMessage(ChatColor.DARK_RED + "[!] Please provide a player!");
+                    return false;
                 }
-            } else {
-                plr0.sendMessage(ChatColor.DARK_RED + "[!] Please specify one of the following options: broadcast, tell");
-            }
+                
+                Player sendTo = Bukkit.getPlayer(args[1]);
 
-            return true;
-        } else return false;
+                plr0.sendMessage(ChatColor.DARK_GREEN + "[~ To " + sendTo.getDisplayName() + "] My coordinates are " + myCoordinates + "! ");
+                sendTo.sendMessage(ChatColor.DARK_AQUA + "[~ From " + sendTo.getDisplayName() + "] My coordinates are " + myCoordinates + "! ");
+                sendTo.playSound(sendTo.getLocation(), Sound.BLOCK_NOTE_BLOCK_BIT, 20F, 1F);
+
+                break;
+            default:
+                plr0.sendMessage(ChatColor.DARK_RED + "[!] Please specify one of the following options: broadcast, tell");
+                break;
+        }
+
+        return true;
     }
 }
